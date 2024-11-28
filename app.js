3da340fd4afd80e90ffb7785f97d16eb201698a2
Adrian <adriansanduta@gmail.com>
2024-11-28 16:52:56 -0600

    2 methods/1.3

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ fetch("https://jsonplaceholder.typicode.com/users/1" )
 .then(response => {
        return response.json()
 }).then(data => {
-    console.log(data)
+    console.log(data);
+    emailRef.innerHTML = data.email
 })
 
 // 2. Async/Await
