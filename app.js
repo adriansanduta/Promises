d92801c02f6973269fdb4d303ddb55f474a5e3ff
Adrian <adriansanduta@gmail.com>
2024-11-28 16:34:01 -0600

    2 methods/1.2

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 //.fetch("https://jsonplaceholder.typicode.com/users/1" )
+const emailRef = document.querySelector(".email")
+console.log(emailRef)
+
 
 // 1. Then
 fetch("https://jsonplaceholder.typicode.com/users/1" ).then(response => {
-       console.log(response.json())
+       response.json().then(data => {
+        console.log(data)
+        emailRef.innerHTML = data.name;
+       })
 })
-
-
-
 
 // 2. Async/Await
