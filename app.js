6dca8153fd1fb9bb9ab78e691df23cfe6a2f3d10
Adrian <adriansanduta@gmail.com>
2024-11-28 17:23:09 -0600

    practice

--- app.js
+++ app.js
@@ -12,11 +12,27 @@ const emailRef = document.querySelector(".email")
 // })
 
 // 2. Async/Await
-async function main() {
-const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-const data = await response.json()
-console.log(data)
-emailRef.innerHTML = data.email
+// async function main() {
+// const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+// const data = await response.json()
+// console.log(data)
+// emailRef.innerHTML = data.email
+// }
+
+// main();
+
+
+function getSubscriptionStatus() {
+    return new Promise((resolve, reject) => {
+        resolve("VIP")
+    })
 }
 
+// 1.Then
+getSubscriptionStatus().then(response => console.log(response))
+
+// 2.Async/Await
+async function main() {
+console.log(getSubscriptionStatus())
+}
 main();
