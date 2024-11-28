4f59011436231627b00921239aecef5d1f2333e4
Adrian <adriansanduta@gmail.com>
2024-11-28 17:47:14 -0600

    practice2.0

--- app.js
+++ app.js
@@ -22,17 +22,62 @@ const emailRef = document.querySelector(".email")
 // main();
 
 
+// function getSubscriptionStatus() {
+//    return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//        resolve("VIP");
+//    }, 2000);
+// });
+// }
+
+// 1.Then
+// getSubscriptionStatus().then(response => console.log(response))
+
+// 2.Async/Await
+// async function main() {
+//     const status = await getSubscriptionStatus();
+//    statusRef.innerHTML = status;
+// }
+// main();
+
+
+/**
+ * 1. Create a function called 'getVideo'
+ * 2. Accept a parameter called 'subscriptionStatus'
+ * 3.Return a new Promise inside of the function that:
+ *  - if "VIP" resolve ("show video")
+ *  - if "FREE" resolve ("show trailer")
+ *  - otherwise reject ("no video")
+ * 4. console.log the result of getVideo(status) in main ()
+ */
+const statusRef = document.querySelector(".status");
 function getSubscriptionStatus() {
-    return new Promise((resolve, reject) => {
-        resolve("VIP")
+        return new Promise((resolve, reject) => {
+             setTimeout(() => {
+            resolve("VIP");
+        }, 2000);
+     });
+     }
+
+
+function getVideo(subscriptionStatus) {
+    return new Promise ((resolve, reject) => {
+        if (subscriptionStatus === "VIP")  {
+            resolve("show video");
+        } 
+        else if (subscriptionStatus === "FREE") {
+                 resolve("show trailer"); 
+                } 
+                else { 
+                    reject("no video");   
+        }
     })
 }
 
-// 1.Then
-getSubscriptionStatus().then(response => console.log(response))
-
-// 2.Async/Await
 async function main() {
-console.log(getSubscriptionStatus())
+    const status = await getSubscriptionStatus();
+    statusRef.innerHTML = status;
+    console.log(await getVideo(status))
 }
+
 main();
